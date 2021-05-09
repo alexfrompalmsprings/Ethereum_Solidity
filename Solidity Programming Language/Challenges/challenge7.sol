552220944625080d30abbc140a5557ffd2de0682
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-05-09 15:15:42 -0700

    complete the challenge #7

--- Solidity Programming Language/Challenges/challenge7.sol
+++ Solidity Programming Language/Challenges/challenge7.sol
@@ -2,12 +2,17 @@
 // Add a function called start() that adds the address of the account that calls it to the dynamic array called players.
 // Deploy and test the contract on Rinkeby Testnet.
 
-
-
 //SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Game{
     address[] public players;
+
+  // Add a function called start() that adds the address of the account that calls it to the dynamic array called players.
+  function start() public{
+    // super similar to a regular pushing on JS
+    // note to add the sender to the players array.
+    players.push(msg.sender);
+  }
 }

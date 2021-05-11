d59e2e6ba87a8a592dfabbf1c26883501158719a
alexfrompalmsprings <alexavila.oc@gmail.com>
2021-05-10 19:23:16 -0700

    create function to get the players entered into the Lottery

--- Solidity Programming Language/Lottery Smart Contract/Lottery.sol
+++ Solidity Programming Language/Lottery Smart Contract/Lottery.sol
@@ -3,15 +3,32 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery{
-  // create an array to hold the addresses of all the players
-  address payable[] public players;
-  address public manager;
 
-  constructor(){
-    manager = msg.sender; // the address of the user interacting with the contract
-  }
+   // declaring the state variables
+    address payable[] public players; //dynamic array of type address payable
+    address public manager;
 
 
+    // declaring the constructor
+    constructor(){
+        // initializing the owner to the address that deploys the contract
+        manager = msg.sender;
+    }
+
+    // declaring the receive() function that is necessary to receive ETH
+    receive () payable external{
+        // each player sends exactly 0.1 ETH
+        // require(msg.value == 0.1 ether);
+        // appending the player to the players array --- convert the address into payable addresses
+        players.push(payable(msg.sender));
+    }
+
+    // returning the contract's balance in wei
+    function getBalance() public view returns(uint){
+        // only the manager is allowed to call it
+        // require(msg.sender == manager);
+        return address(this).balance;
+    }
 
 
 
